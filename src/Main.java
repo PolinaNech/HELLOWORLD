07ccfc3630640891c68650269abb21871ea18d64
ME <apolinariy-2001@mail.ru>
2023-11-01 00:00:27 +0300

    HW3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет Мир.");
+        System.out.println("Задание №1");
+        byte a = 127;
+        short b = 200;
+        int c = 40000;
+        long d = 3000000L;
+        float e = 3.14F;
+        double f = 1.12345678;
+        System.out.println("Значение переменной a с типом byte равно " + a);
+        System.out.println("Значение переменной b с типом short равно " + b);
+        System.out.println("Значение переменной c с типом int равно " + c);
+        System.out.println("Значение переменной d с типом long равно " + d);
+        System.out.println("Значение переменной e с типом float равно " + e);
+        System.out.println("Значение переменной f с типом double равно " + f);
+        System.out.println("Задание №2");
+        float g = 27.12F;
+        long h = 987678965549L;
+        float i = 2.786F;
+        short j = 569;
+        short k = -159;
+        short  l = 27897;
+        byte m = 67;
+        System.out.println("Задание №2");
+        byte lP = 23;
+        byte aS = 27;
+        byte eA= 30;
+        short allPaper = 480;
+        int studentPaper = allPaper/(lP + aS + eA);
+        System.out.println("На каждого ученика рассчитано " + studentPaper + " листов бумаги");
+
     }
 }
