9a907c13c1f6f50885842bc65185bbff9dc44a67
ME <apolinariy-2001@mail.ru>
2023-11-26 19:53:25 +0300

    Home work 10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет Мир.");
+        System.out.println("Домашнее задание № 10. Тема: Строки");
+        System.out.println("Задача №1");
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("Ф. И. О. сотрудника - " + fullName);
+        System.out.println("Задача №2");
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        System.out.println("Задача №3");
+        fullName = "Иванов Семён Семёнович";
+        String newFullName = fullName.replace('ё','е');
+        System.out.println("Данные Ф. И. О. сотрудника - " + newFullName);
     }
 }
