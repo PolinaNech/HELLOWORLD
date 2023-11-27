31579000ef0c33d87d98d262c1b1391f6d31631c
ME <apolinariy-2001@mail.ru>
2023-11-27 21:45:34 +0300

    HW11

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет Мир.");
+        System.out.println("Домашнее задание № 11. Тема: Методы");
+        System.out.println("Задача №1");
+        System.out.println("Задача №2");
     }
 }
