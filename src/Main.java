c9a9e3fa2f2168b1e8fb058f6d1fa1a9ce961acb
ME <apolinariy-2001@mail.ru>
2023-11-27 21:58:52 +0300

    HW11

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 public class Main {
+    public static void countingLeapYear(int year){
+        if ((year % 4 == 0 && year % 100 !=0) || (year % 400 == 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Домашнее задание № 11. Тема: Методы");
         System.out.println("Задача №1");
+        int year = 2021;
+        countingLeapYear(year);
         System.out.println("Задача №2");
+        System.out.println("Задача №3");
     }
 }
