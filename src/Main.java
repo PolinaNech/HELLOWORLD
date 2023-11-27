337a832183d0969109d4432b60d717b3614f5814
ME <apolinariy-2001@mail.ru>
2023-11-27 22:07:20 +0300

    HW11

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,31 @@ public class Main {
             System.out.println(year + " год не является високосным");
         }
     }
+    public static void calculationVersionDevice(int clientDeviceYear, int typeSystem){
+        boolean oldVersion = clientDeviceYear < 2015;
+        boolean iosType = typeSystem == 0;
+        boolean androidType = typeSystem == 1;
+        if (oldVersion && iosType){
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (iosType){
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }  else if (oldVersion && androidType){
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (androidType){
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Для данного вида операционной системы приложение пока не предусмотренно.");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Домашнее задание № 11. Тема: Методы");
         System.out.println("Задача №1");
         int year = 2021;
         countingLeapYear(year);
         System.out.println("Задача №2");
+        int clientDeviceYear = 2010;
+        int typeSystem = 1;
+        calculationVersionDevice(clientDeviceYear,typeSystem);
         System.out.println("Задача №3");
     }
 }
