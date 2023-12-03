3ca5b8ab92e9f30249693f110b7b6a54a43e0d83
ME <apolinariy-2001@mail.ru>
2023-12-03 20:18:32 +0300

    Homework with new classes

---
+++ src/Author.java
@@ -0,0 +1,11 @@
+public class Author {
+    String author;
+
+    public Author(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
---
+++ src/Book.java
@@ -0,0 +1,16 @@
+public class Book {
+    int pageAmount;
+    String publisher;
+
+    public Book(String author, int pageAmount, String publisher) {
+        this.pageAmount = pageAmount;
+        this.publisher = publisher;
+    }
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+}
