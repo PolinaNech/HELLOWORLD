f3d405c37a1f0c40e52efe6834d7315c373e2d18
ME <apolinariy-2001@mail.ru>
2023-12-05 12:11:55 +0300

    Homework with new classes

--- src/Book.java
+++ src/Book.java
@@ -1,11 +1,17 @@
 public class Book {
-    String author;
+    Author author ;
     int pageAmount;
     String publisher;
 
-    public Book( String author, int pageAmount, String publisher) {
+    public Book( String author , int pageAmount, String publisher) {
         this.pageAmount = pageAmount;
         this.publisher = publisher;
+    }
+    public int getPageAmount() {
+        return this.pageAmount;
+    }
+    public void setPageAmount() {
+        this.pageAmount = pageAmount;
     }
     public String getPublisher() {
         return publisher;
