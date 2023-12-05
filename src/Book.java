44ee380f3043b97b41a54ee359819fc42e740e59
PolinaNech <148264458+PolinaNech@users.noreply.github.com>
2023-12-06 01:05:11 +0300

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,26 +1,26 @@
 public class Book {
-    Author author ;
-    int pageAmount;
-    String published;
-    String title;
+        Author author ;
+        int pageAmount;
+        int published;
+        String title;
 
-    public Book( String author , int pageAmount, String published, String title) {
-        this.pageAmount = pageAmount;
-        this.published = published;
-        this.title = title;
-    }
-    public int getPageAmount() {
-        return this.pageAmount;
-    }
-    public String getPublished() {
-        return published;
-    }
+        public Book( String author , int pageAmount, int published, String title) {
+            this.pageAmount = pageAmount;
+            this.published = published;
+            this.title = title;
+        }
+        public int getPageAmount() {
+            return this.pageAmount;
+        }
+        public int getPublished() {
+            return published;
+        }
 
-    public void setPublished(String published) {
-        this.published = published;
+        public void setPublished(int published) {
+            this.published = published;
 
+        }
+        public String getTitle() {
+            return title;
+        }
     }
-    public String getTitle() {
-        return title;
-    }
-}
