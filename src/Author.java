142becbf63526e48b26cdd9e3c3c702183cb0e96
PolinaNech <148264458+PolinaNech@users.noreply.github.com>
2023-12-05 11:36:59 +0300

    Update Author.java

--- src/Author.java
+++ src/Author.java
@@ -1,11 +1,20 @@
 public class Author {
     String author;
-
+    String name;
+    String surname;
+    public Author(String name,  String surname) {
+        this.name = name;
+        this.surname = surname;
+        this.author = name + surname;
+    }
     public Author(String author) {
         this.author = author;
     }
 
     public String getAuthor() {
         return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
     }
 }
