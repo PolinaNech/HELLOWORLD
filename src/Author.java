52c7a70469c786a21feccbd05af70f89f110fd8a
PolinaNech <148264458+PolinaNech@users.noreply.github.com>
2023-12-09 15:08:46 +0300

    Update Author.java

--- src/Author.java
+++ src/Author.java
@@ -1,12 +1,10 @@
 import java.util.Objects;
 public class Author{
-    String author;
-    String name;
-    String surname;
+    private String name;
+    private String surname;
     public Author(String name,  String surname) {
         this.name = name;
         this.surname = surname;
-        this.author = name + surname;
     }
     public String getName() {
         return name;
@@ -20,17 +18,28 @@ public class Author{
     public void setSurname(String surname) {
         this.surname = surname;
     }
-    public boolean equals(Object other) {
-        if (this.getClass() != other.getClass()) {
-            return false;
-        }
-        return author.equals(name + surname);
-    }
-    public int hashCode() {
-        return java.util.Objects.hash(author);
-    }
-    public String toString(){
-        return "Автор: " + author;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author1 = (Author) o;
+        return Objects.equals(name, author1.name) && Objects.equals(surname, author1.surname);
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+
+    public String toString(){
+        return "Автор: " + name + " "  + surname;
+    }
+
+}
+    public String toString(){
+        return author + "Количество страниц: " + pageAmount + " Год публикации: " + published + " Книга:" + title;
+    }
+
+}
 }
