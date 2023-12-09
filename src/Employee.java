a3496dc76d3b6726bed7db4d24053cdf25fa06c7
ME <apolinariy-2001@mail.ru>
2023-12-09 18:28:37 +0300

    Coursework1

---
+++ src/Employee.java
@@ -0,0 +1,47 @@
+import java.util.Objects;
+public class Employee  {
+    private static int id = 0;
+    private String surname;
+    private String name;
+    private String paternity;
+    private int section;
+    private int salary;
+    public int sum;
+    public Employee (String surname, String name, String paternity, int section, int salary){
+        id=id+1;
+        this.surname=surname;
+        this.name=name;
+        this.paternity=paternity;
+        this.section=section;
+        this.salary=salary;
+    }
+
+    public int getId(){
+        return id;
+    }
+    public String getSurname(){
+        return surname;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getPaternity(){
+        return paternity;
+    }
+    public int getSection(){
+        return section;
+    }
+    public void setSection(int getSection){
+        this.section=section;
+    }
+    public int getSalary(){
+        return salary;
+    }
+    public void setSalary(int getSalary){
+        this.salary=salary;
+    }
+    @Override
+    public String toString(){
+        return "ID: " + id + ". Ф.И.О: " + surname + " " + name + " " + paternity + ". Отдел: " + section + ". Заработная плата: " + salary + " рублей.";}
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет Мир.");
+        Employee frame1 = new Employee("Ivanov","Ivan","Ivanovich",2,70000);
+        System.out.println(frame1);
+        Employee frame2 = new Employee("Ivanova","Anna","Ivanovna",4,75000);
+        System.out.println(frame2);
     }
 }
