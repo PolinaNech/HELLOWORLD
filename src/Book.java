c5fb806a37d8b6d2b410e32b456aa3ede9471898
PolinaNech <148264458+PolinaNech@users.noreply.github.com>
2023-12-09 15:09:42 +0300

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,14 +1,20 @@
-public class Book {
-        Author author;
-        int pageAmount;
-        int published;
-        String title;
+import java.util.Objects;
 
-        public Book( String author, int pageAmount, int published, String title) {
+public class Book {
+    private Author author;
+    private int pageAmount;
+    private int published;
+    private String title;
+
+        public Book( Author author, int pageAmount, int published, String title) {
+            this.author = author;
             this.pageAmount = pageAmount;
             this.published = published;
             this.title = title;
         }
+        public Author getAuthor() {
+        return this.author;
+    }
         public int getPageAmount() {
             return this.pageAmount;
         }
@@ -23,17 +29,22 @@ public class Book {
         public String getTitle() {
             return title;
         }
-    public boolean equals(Object other) {
-        if (this.getClass() != other.getClass()) {
-            return false;
-        }
-        return title.equals(title);
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return pageAmount == book.pageAmount && published == book.published && Objects.equals(author, book.author) && Objects.equals(title, book.title);
     }
+
+    @Override
     public int hashCode() {
-        return java.util.Objects.hash(title);
+        return Objects.hash(author, pageAmount, published, title);
     }
+
     public String toString(){
-        return "Количество страниц: " + pageAmount + " Год публикации: " + published + " Книга:" + title;
+        return author + "Количество страниц: " + pageAmount + " Год публикации: " + published + " Книга:" + title;
     }
 
 }
