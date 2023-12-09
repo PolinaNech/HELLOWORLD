99016e39d1e7a0676ae8428f173fe9377b7f34dd
PolinaNech <148264458+PolinaNech@users.noreply.github.com>
2023-12-09 15:10:20 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Домашнее задание № 12.Тема: Объекты и классы.");
-        Author author1 = new Author("Stephen"," King ");
-        Book book1= new Book( "Stephen King ",1248, 1986, " It");
-        System.out.println(author1 + " " + book1 );
-        Author author2 = new Author("Leo"," Tolstoy ");
-        Book book2= new Book("Leo Tolstoy ", 1300, 1869, " War and peace");
-        System.out.println(author2 + " " + book2 );
+        Author author1 = new Author("Stephen","King ");
+        Book book1= new Book( author1,1248, 1986, " It");
+        System.out.println( book1);
+        Author author2 = new Author("Leo","Tolstoy ");
+        Book book2= new Book(author2, 1300, 1869, " War and peace");
+        System.out.println(book2);
 
     }
 }
