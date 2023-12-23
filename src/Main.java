2168c1b4f82fd0ce432cea126798feb2c18b442d
ME <apolinariy-2001@mail.ru>
2023-12-23 21:00:27 +0300

    Homework 13

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
 import java.util.Arrays;
 
 public class Main {
+
+
     private static void changeValue(Integer[] value) {
     value = new Integer [] {99};
-
 
 }
     public static void main(String[] args) {
         Integer [] value = {3,4};
         changeValue(value);
         System.out.println(Arrays.toString(value));
-
+        Person person = new Person("Lyapis ","Trubetskoy");
+        changePerson(person);
+        System.out.println(person);
     }
 
-
+    private static void changePerson(Person person){
+        person = new Person("Ilya ","Lagutenko");
+    }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,6 +1,10 @@
 public class Person {
-    private String name;
-    private String surname;
+    public static String name;
+    public static String surname;
+    public Person(String name,  String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
     public String toString(){
         return "Автор: " + name + " "  + surname;
     }
