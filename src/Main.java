139b29a23f71cdec94dbe249d0d403282ba73418
ME <apolinariy-2001@mail.ru>
2023-12-23 20:03:09 +0300

    Homework 13

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+
 public class Main {
+    private static void changeValue(Integer[] value) {
+    value = new Integer [] {99};
+
+
+}
     public static void main(String[] args) {
-        System.out.println("Привет Мир.");
+        Integer [] value = {3,4};
+        changeValue(value);
+        System.out.println(Arrays.toString(value));
+
     }
+
+
 }
