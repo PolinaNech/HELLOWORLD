fa05a998383bf41fc6d260507a94f7e26b16172e
ME <apolinariy-2001@mail.ru>
2024-01-14 21:33:57 +0300

    Homework 2.3

---
+++ src/Car.java
@@ -0,0 +1,6 @@
+public class Car extends Bicycle{
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+}
---
+++ src/Truck.java
@@ -0,0 +1,10 @@
+public class Truck extends Bicycle{
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+}
